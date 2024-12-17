390e93267b84868b2f9a77aa9c674d5f593dcae7
Lily Mara <lilymara@fastmail.com>
2024-12-17 11:31:23 -0800

    wait for init to finish before proceeding, fixes bug in first startup

--- server/plugins/url-scrape.ts
+++ server/plugins/url-scrape.ts
@@ -2,7 +2,7 @@ import { DateTime } from "luxon";
 import { doUrlScrapes, initializeAllScrapers } from '~~/server/utils/http';
 
 export default defineNitroPlugin(async (nitroApp) => {
-	initializeAllScrapers();
+	await initializeAllScrapers();
 
 	// Run the url scrape job every 10 minutes, it skips organizers that have been updated in the past hour. It's
 	// re-run every 10 minutes so that if there were errors for any one organizer the last time it was run, it'll try
