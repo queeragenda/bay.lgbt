cdd88dd096ae6f88bb10608e85a0d5dc5f3fd20f
Lily Mara <lilymara@fastmail.com>
2025-01-13 10:12:50 -0800

    skip scraping if variable tells it to

--- server/plugins/instagram-scrape.ts
+++ server/plugins/instagram-scrape.ts
@@ -1,6 +1,7 @@
 import { getOrInsertOrganizer, scrapeInstagram } from "~~/server/utils/instagram";
 import eventSourcesJSON from '~~/server/utils/event_sources.json';
 import { DateTime } from "luxon";
+import { logger } from '../utils/logger';
 
 export default defineNitroPlugin(async (nitroApp) => {
 	await Promise.all(eventSourcesJSON.instagram.map(source => getOrInsertOrganizer(source)));
@@ -8,6 +9,10 @@ export default defineNitroPlugin(async (nitroApp) => {
 	// Run the instagram scrape job every 60 minutes, it skips organizers that have been updated in the past two hours. It's
 	// re-run every hour so that if there were errors for any one organizer the last time it was run, it'll try
 	// again.
+	if (process.env.SKIP_SCRAPE === 'true') {
+		logger.warn('Skipping Instgram scrapes due to SKIP_SCRAPE env var');
+		return;
+	}
 	setInterval(() => runScrape(), 1000 * 60 * 10);
 
 	runScrape();
--- server/plugins/url-scrape.ts
+++ server/plugins/url-scrape.ts
@@ -7,6 +7,11 @@ export default defineNitroPlugin(async (nitroApp) => {
 	// Run the url scrape job every 10 minutes, it skips organizers that have been updated in the past hour. It's
 	// re-run every 10 minutes so that if there were errors for any one organizer the last time it was run, it'll try
 	// again.
+	//
+	if (process.env.SKIP_SCRAPE === 'true') {
+		logger.warn('Skipping URL scrapes due to SKIP_SCRAPE env var');
+		return;
+	}
 	setInterval(() => runScrape(), 1000 * 60 * 10);
 
 	runScrape();
