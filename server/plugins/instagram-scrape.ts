c11cde406dfe85d49f2705ac916e2671515506d0
Lily Mara <lilymara@fastmail.com>
2025-01-12 20:08:01 -0800

    instagram re-scrape 2h, existing scrape was leading to always having exhausted rate limit

--- server/plugins/instagram-scrape.ts
+++ server/plugins/instagram-scrape.ts
@@ -5,8 +5,8 @@ import { DateTime } from "luxon";
 export default defineNitroPlugin(async (nitroApp) => {
 	await Promise.all(eventSourcesJSON.instagram.map(source => getOrInsertOrganizer(source)));
 
-	// Run the instagram scrape job every 10 minutes, it skips organizers that have been updated in the past hour. It's
-	// re-run every 10 minutes so that if there were errors for any one organizer the last time it was run, it'll try
+	// Run the instagram scrape job every 60 minutes, it skips organizers that have been updated in the past two hours. It's
+	// re-run every hour so that if there were errors for any one organizer the last time it was run, it'll try
 	// again.
 	setInterval(() => runScrape(), 1000 * 60 * 10);
 
@@ -14,5 +14,5 @@ export default defineNitroPlugin(async (nitroApp) => {
 })
 
 function runScrape() {
-	scrapeInstagram({ onlyUpdateStalerThan: DateTime.now().minus({ hours: 1 }) });
+	scrapeInstagram({ onlyUpdateStalerThan: DateTime.now().minus({ hours: 2 }) });
 }
