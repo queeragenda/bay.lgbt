b581cd2972920f12769927f4e814dd7e0873acd0
Lily Mara <lilymara@fastmail.com>
2025-01-15 14:22:54 -0800

    add ig-specific scrape skip var

--- server/plugins/instagram-scrape.ts
+++ server/plugins/instagram-scrape.ts
@@ -9,7 +9,7 @@ export default defineNitroPlugin(async (nitroApp) => {
 	// Run the instagram scrape job every 60 minutes, it skips organizers that have been updated in the past two hours. It's
 	// re-run every hour so that if there were errors for any one organizer the last time it was run, it'll try
 	// again.
-	if (process.env.SKIP_SCRAPE === 'true') {
+	if (process.env.SKIP_SCRAPE === 'true' || process.env.SKIP_INSTAGRAM_SCRAPE === 'true') {
 		logger.warn('Skipping Instgram scrapes due to SKIP_SCRAPE env var');
 		return;
 	}
