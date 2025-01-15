514b764c9e506ea4f0988aa705e58f09da88fc3a
Lily Mara <lilymara@fastmail.com>
2025-01-14 21:36:09 -0800

    try showing an image?

--- pages/[organizer_type]/[organizer]/[event_id]/[event_slug].vue
+++ pages/[organizer_type]/[organizer]/[event_id]/[event_slug].vue
@@ -18,10 +18,12 @@ if (data?.value?.body) {
 
   const start = new Date(event.start);
   const restDescription = event.extendedProps.description ? ` // ${event.extendedProps.description}` : '';
+  const image = event.extendedProps.images ? event.extendedProps.images[0] : null;
 
   useSeoMeta({
     ogTitle: event.title,
     ogDescription: `${start.toLocaleDateString('en-US', { timeZone: 'America/Los_Angeles' })} @ ${start.toLocaleTimeString('en-US', { timeZone: 'America/Los_Angeles' })}${restDescription}`,
+    ogImage: image,
   });
 }
 
